f5ccae2672a48f87b22d2f93f1b63995a5c0b8a0
Sedranadr117 <sedranadr117@gmail.com>
2025-09-04 11:30:15 +0300

    pull merging

--- lib/features/products/search_product/presentation/binding/search_product_binding.dart
+++ lib/features/products/search_product/presentation/binding/search_product_binding.dart
@@ -1,13 +1,9 @@
 import 'package:get/get.dart';
-import 'package:teriak/features/products/search_product/presentation/controller/search_product_controller.dart';
 import 'package:teriak/features/suppliers/all_supplier/presentation/controller/all_supplier_controller.dart';
 
 class SearchProductBinding extends Bindings {
   @override
   void dependencies() {
-    Get.put<SearchProductController>(SearchProductController(),
-        permanent: true);
-    Get.put<GetAllSupplierController>(GetAllSupplierController(),
-        permanent: true);
+    Get.put<GetAllSupplierController>(GetAllSupplierController(),permanent: true);
   }
 }
